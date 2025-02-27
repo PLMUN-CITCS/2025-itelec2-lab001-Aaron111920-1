a1a439540c6fbc8052170ccb59a262f76e451be9
Aaron1119 <crisostomoaaronluis_bsit@plmun.edu.ph>
2025-02-27 21:33:49 +0800

    My first Python program

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
 
+print("Hello world!")
+print("Hello world!")
+print("Name: Crisostomo, Aaron Luis C.")
+print("Course: Information Technology")
+print("Interests in programming: Making Website")
+
